e024f9ae2dab58acdce7722357ba432d93f39df7
jennie <jennie.tompowsky@hotmail.com>
2024-10-03 12:17:04 +0200

    added surface controller and style for home page form

---
+++ UmbracoCms/Controllers/RequestSurfaceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoCms.Models;
+
+namespace UmbracoCms.Controllers;
+
+public class RequestSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+{
+    public IActionResult HandleSubmit(RequestFormModel form)
+    {
+        if(!ModelState.IsValid)
+        {
+            ViewData["name"] = form.Name;
+            ViewData["email"] = form.Email;
+            ViewData["phone"] = form.Phone;
+            ViewData["option"] = form.SelectedOption;
+
+
+            ViewData["error_name"] = string.IsNullOrEmpty(form.Name);
+            ViewData["error_email"] = string.IsNullOrEmpty(form.Email);
+            ViewData["error_phone"] = string.IsNullOrEmpty(form.Phone);
+            ViewData["error_option"] = string.IsNullOrEmpty(form.SelectedOption);
+
+            return CurrentUmbracoPage();
+        }
+
+        TempData["success"] = "Request send successfully";
+        return CurrentUmbracoPage();
+    }
+}
---
+++ UmbracoCms/Models/RequestFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UmbracoCms.Models;
+
+public class RequestFormModel
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+    public string SelectedOption { get; set; } = null!;
+}
